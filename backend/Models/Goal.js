b77bce349ad08ce61f4b708551275e918adde5b4
Antonio <antonioruiz2606@gmail.com>
2022-10-10 19:25:45 -0400

    Added Goal model and a simple test in index.js

--- backend/Models/Goal.js
+++ backend/Models/Goal.js
@@ -1,1 +1,45 @@
-// TODO: Implement the Goal Model
+import mongoose from 'mongoose';
+
+export default mongoose.model('Goals', new mongoose.Schema({
+    // Required Fields
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    goalType: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    priorityValue: {
+        type: Number,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true,
+        default: () => Date.now()
+    },
+    endDate: {
+        type: Date,
+        required: true,
+    },
+
+    // Optional Fields
+    creationDate: {
+        type: Date,
+        immutable: true,
+        default: () => Date.now()
+    },
+
+    // Relations:
+    creatorId: mongoose.SchemaTypes.ObjectId,
+    commentIDs: [mongoose.SchemaTypes.ObjectId]
+}), 'Goals');
