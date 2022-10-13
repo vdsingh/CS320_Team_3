a700e594d043669624b69151de93447a944a11f2
Antonio Ruiz Fernandez <antonioruiz2606@gmail.com>
2022-10-13 14:21:41 -0400

    Progress

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,11 +1,11 @@
-const jwt = require("jsonwebtoken");
-exports.createJWT = (email, userId, duration) => {
+import { sign } from "jsonwebtoken";
+export function createJWT(email, userId, duration) {
    const payload = {
       email,
       userId,
       duration
    };
-   return jwt.sign(payload, process.env.TOKEN_SECRET, {
+   return sign(payload, process.env.TOKEN_SECRET, {
      expiresIn: duration,
    });
-};
+}
