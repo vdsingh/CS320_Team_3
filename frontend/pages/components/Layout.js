785c5b01bc43b0cc3f4e2263235b07ef230f7355
pmerino2001 <pmerino@umass.edu>
2022-10-17 20:50:19 -0400

    Updated Navabrs

--- frontend/pages/components/Layout.js
+++ frontend/pages/components/Layout.js
@@ -1,11 +1,13 @@
 import LoginNav from '../components/Navbars/LoginNav'
+import HomeNav from '../components/Navbars/HomeNav'
 
 const Layout = ({ children, navbarType }) => {
     return (
         //Here is where You can put different navbars for different windows
         //navbarType 1 is the navbar for the login page
         <div>
-            { navbarType == 1 &&  <LoginNav />}
+            {navbarType == 1 &&  <LoginNav />}
+            {navbarType == 2 &&  <HomeNav />}
             {children}
             
         </div>
