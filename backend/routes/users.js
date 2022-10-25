109e62d0e683488a8003b0c6682657f8bd13f008
Charvi Attri <64918960+charvi-a@users.noreply.github.com>
2022-10-25 13:49:24 -0400

    Added updateUser functionality

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,42 @@
 import User from '../Models/User.js'
 
-export default function read_user(req, res){
-    const {email, password} = req.body;
-
-    User.findOne({email: email}, (err, user) =>{
-        if (err) return res.status(500).send(err)
-        return res.status(200).send(user);
-    });
+export default function readUserById(req, res){
+    const { userId } = req.params;
+   
+    User.findById(userId, (err, user) => {
+        if (err) {
+            res.status(500).send(err);
+        } else if (user) {
+            res.status(200).send({ message: "Successfully retrieved user", user: user });
+        } else {
+            res.status(404).send("The user does not exist.");
+        }
+    })
 }
+
+export default function updateUser(req, res){
+    const {userId} = req.params;
+    const = {
+        firstName, 
+        lastName, 
+        email, 
+        password,
+    } = req.body;
+    
+    User.findByIdAndUpdate(
+        userId,  
+        { "firstName": firstName, "lastName": lastName, "email": email, "password": password}, 
+        (err, user) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(err);
+        } else if (user){
+            res.status(200).send({message: "Successfully updated user.", user: user});
+        } else {
+            res.status(404).send("The user does not exist.");
+        }
+     });
+}
+
+
+
