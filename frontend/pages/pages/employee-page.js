9bac06e1504fb810867a843033f8612c977f3626
unknown <chloex2000@gmail.com>
2022-10-25 13:25:37 -0400

    Trying to resolve merge conflict by adding login-validator

--- frontend/pages/pages/employee-page.js
+++ frontend/pages/pages/employee-page.js
@@ -5,31 +5,34 @@ import Layout from '../components/Layout'
 import GoalForm from '../components/GoalForm'
 import ManagerInfo from '../components/ManagerInfo'
 import EmployeeName from '../components/EmployeeName'
+import loginValidator from './login-validator'
 
 export default function employeePage(){
-    return(
-        <Layout navbarType={3}>
-        <div>
-            <Head>
-                <title>Employee Page</title>
-                <link rel="icon" href="\UKG_(Ultimate_Kronos_Group)_logo.svg.png" />
-            </Head>
+    if (loginValidator()){
+        return(
+            <Layout navbarType={3}>
             <div>
-                <h1>
-                    <GoalForm />
-                </h1>
+                <Head>
+                    <title>Employee Page</title>
+                    <link rel="icon" href="\UKG_(Ultimate_Kronos_Group)_logo.svg.png" />
+                </Head>
+                <div>
+                    <h1>
+                        <GoalForm />
+                    </h1>
+                </div>
+                <div>
+                    <h2> 
+                        <EmployeeName />
+                    </h2>
+                </div>
+                <div>
+                    <h1>
+                        <ManagerInfo />
+                    </h1>
+                </div>
             </div>
-            <div>
-                <h2> 
-                    <EmployeeName />
-                </h2>
-            </div>
-            <div>
-                <h1>
-                    <ManagerInfo />
-                </h1>
-            </div>
-        </div>
-        </Layout>
-    )
+            </Layout>
+        )
+    }
 }
