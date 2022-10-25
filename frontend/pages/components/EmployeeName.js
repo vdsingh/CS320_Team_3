510bc8f968cac49b3237630acc27d20d90ddc36d
Steve Dang <adang@umass.edu>
2022-10-25 15:37:03 -0400

    update emp name from cookies

--- frontend/pages/components/EmployeeName.js
+++ frontend/pages/components/EmployeeName.js
@@ -2,19 +2,25 @@ import React from 'react';
 import styles from '../../styles/employee.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { getCookie } from 'cookies-next'
 
-var testEmp1 = {
-    name: 'Arthur Read',
-    position_title: 'Principal Engineering Manager'
+const getEmployeeInfo = () =>{
+    var loginInfo = JSON.parse(getCookie("login"))
+    var emp = {
+        firstname: loginInfo.user.firstName,
+        lastname: loginInfo.user.lastName,
+        fullname: loginInfo.user.firstName +" "+ loginInfo.user.lastName,
+        position_title: 'Principal Engineering Manager'
+    }
+    return emp
 }
-
 export default function EmployeeName(){
     const router = useRouter()
 
     return(
         <div className={styles.emp_box}>
-        <h1 className={styles.emp_name}>{testEmp1.name}</h1>
-        <h2 className={styles.job_title}>{testEmp1.position_title}</h2>
+        <h1 className={styles.emp_name}>{getEmployeeInfo().fullname}</h1>
+        <h2 className={styles.job_title}>{getEmployeeInfo().position_title}</h2>
         </div>
     )
 }
