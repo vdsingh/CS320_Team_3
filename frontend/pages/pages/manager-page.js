fd678acefb70aabf20519e4a0585f338cd99a2bd
Steve Dang <adang@umass.edu>
2022-11-08 05:26:04 -0500

    create goal pop-up

--- frontend/pages/pages/manager-page.js
+++ frontend/pages/pages/manager-page.js
@@ -8,7 +8,10 @@ import ReportInfo from '../components/ReportInfo'
 import EmployeeName from '../components/EmployeeName'
 import loginValidator from './login-validator'
 import styles from '../../styles/employee.module.css'
+import { useRouter } from 'next/router'
+
 export default function employeePage(){
+    const router = useRouter()
     if (loginValidator()){
         return(
             <Layout navbarType={4}>
@@ -29,7 +32,7 @@ export default function employeePage(){
                     </h2>
                 </div> */}
                 <div>
-                    <button className={styles.link} onClick={() => router.push('/pages/goals')}>Click Here to See All...</button>
+                    <button className={styles.link} onClick={() => router.push('/pages/goal-page')}>Click Here to See All...</button>
                     <h1>
                         <ManagerInfo />
                     </h1>
