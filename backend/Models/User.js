4d63ddfefd49188d0936d8ecf32ff096440f5f2e
Antonio Ruiz Fernandez <antonioruiz2606@gmail.com>
2022-11-08 15:40:31 -0500

    changes

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,50 +1,62 @@
 import mongoose, { trusted } from "mongoose";
 
-export default mongoose.model("Users", new mongoose.Schema({
-    // Required Fields:
-    //updatable
-    firstName: {
-      type: String,
-      required: true,
-    },
-    //updatable
-    lastName: {
-      type: String,
-      required: true,
-    },
-    //updatable
-    email: {
-      type: String,
-      required: true,
-      lowercase: true,
-    },
-    //updatable
-    password: {
-      type: String,
-      required: true,
-    },
-    employeeId: {
-      type: Number,
-      required: true,
-    },
-    //not updatable
-    isManager: {
-      type: Boolean,
-      required: true,
-      immutable: true,
-    },
-    //not updatable
-    companyId: {
-      type: Number,
-      required: true,
-    },
+const user_schema = new mongoose.Schema({
+  // Required Fields:
+  //updatable
+  firstName: {
+    type: String,
+    required: true,
+  },
+  //updatable
+  lastName: {
+    type: String,
+    required: true,
+  },
+  //updatable
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+  //updatable
+  password: {
+    type: String,
+    required: true,
+  },
+  employeeId: {
+    type: Number,
+    required: true,
+  },
+  //not updatable
+  isManager: {
+    type: Boolean,
+    required: true,
+    immutable: true,
+  },
+  //not updatable
+  companyId: {
+    type: Number,
+    required: true,
+  },
 
-    // Non-required Fields:
-    companyName: String,
-    positionTitle: String,
-    startDate: String,
-    managerId: Number,
-    // Relations:
-    managerUId: mongoose.SchemaTypes.ObjectId
+  // Non-required Fields:
+  companyName: String,
+  positionTitle: String,
+  startDate: String,
+  managerId: Number,
+  // Relations:
+  managerUId: mongoose.SchemaTypes.ObjectId,
+});
+
+// Composite index by managerId and companyId for fast reportee querying 
+user_schema.index(
+  {
+    managerId: 1,
+    companyId: 1,
+  },
+  {
+    unique: true,
   }
-));
+);
+
+export default mongoose.model("Users", user_schema, "Users");
