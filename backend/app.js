52e3de473855138586c72982bed9b799de772da5
Vikram Singh <>
2022-11-08 19:37:58 -0500

    Linting

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import createServer from "./utils/server.js";
 import connect from "./utils/connect.js";
 
 // To run the server as normal, simply use "npm run dev" in the terminal
-
 const app = createServer();
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
