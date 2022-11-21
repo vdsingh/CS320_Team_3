60e0b327d300e46e78deeaa8d8899937cb0a86a6
Vikram Singh <>
2022-11-20 21:18:47 -0500

    Add more login tests

--- backend/__tests__/login.test.js
+++ backend/__tests__/login.test.js
@@ -1,11 +1,7 @@
 import request from 'supertest';
 import createServer from '../utils/server';
 import dotenv from 'dotenv';
-// import login from '../routes/login';
-
 import mongoose from "mongoose";
-
-// NODE_OPTIONS=--experimental-vm-modules npx jest
 
 const app = createServer();
 /* Connecting to the database before each test. */
@@ -20,30 +16,30 @@ afterEach(async () => {
 });
 
 describe("POST /login", () => {
-
     describe("given a username and password", () => {
-        //should save username and password to database
-        //should respond with a json object that contains a success message and the user object
-        //should respond with a 200 status code
-        test("should respond with a 200 status code", async () => {
+        test("correct credentials: should respond with a 200 status code and json header", async () => {
             const response = await request(app).post("/api/login").send({
                 email: "vdsingh@umass.edu",
                 password: "password"
             });
             expect(response.statusCode).toBe(200);
+            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
         });
-        //should specify json in the content type header
-        // test("should specify json in the content type header", async () => {
-        //     const response = await request(app).post("/api/login").send({
-        //         email: "vdsingh@umass.edu",
-        //         password: "password"
 
-        //     });
-        //     expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
-        // });
+        test("incorrect credentials: should respond with a 401 status code", async () => {
+            const response = await request(app).post("/api/login").send({
+                email: "invalid",
+                password: "invalid"
+            });
+            expect(response.statusCode).toBe(401);
+        });
     });
 
     describe("when the username and password is missing", () => {
         //should respond with a status code of 401
+        test("incorrect request: should respond with a 401 status code", async () => {
+            const response = await request(app).post("/api/login").send({});
+            expect(response.statusCode).toBe(401);
+        });
     });
 });
