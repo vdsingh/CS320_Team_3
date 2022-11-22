56a71964bb40a8696f9ca5e7e9b55a1d29d4289b
achoreim-umass <achoreim@umass.edu>
2022-11-22 13:51:27 -0500

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -2,9 +2,11 @@
 import login from "./routes/login.js";
 import { findUserByManagerIDandCompanyID    , readUserById, updateUserById } from "./routes/users.js"
 import { createGoal, readGoalById, updateGoalById, deleteGoalById, readUserGoals } from "./routes/goals.js"
+import { createComment, readGoalComments, readCommentById, updateCommentById, deleteCommentById} from "./routes/comments.js";
 
 
 export default function routes(app) {
+
     /**
      * CRUD for users
      */
@@ -21,6 +23,15 @@ export default function routes(app) {
     app.delete("/api/goals/byGoalId/:goalId", deleteGoalById);
 
     app.get("/api/goals/byUserId/:userId", readUserGoals);
+
+    /**
+     * CRUD for comments
+     */
+    app.post("/api/comments", createComment);
+    app.get("/api/comments/byGoalId/:goalId", readGoalComments);
+    app.get("/api/comments/byCommentId/:commentId", readCommentById);
+    app.put("/api/comments/byCommentId/:commentId", updateCommentById);
+    app.delete("/api/comments/:commentId", deleteCommentById);
 
     /**
      * Log in, uses the imported signin function from the routes folder.
