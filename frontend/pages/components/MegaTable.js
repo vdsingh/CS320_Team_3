ad6cc5992d6cd64d5550acfd89e7dc9b8e01635f
Sammy Chen <sammychen10@yahoo.com>
2022-11-22 14:37:20 -0500

    mega tbl integration

--- frontend/pages/components/MegaTable.js
+++ frontend/pages/components/MegaTable.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
-import {DataGrid} from '@material-ui/data-grid'
+import { DataGrid } from '@material-ui/data-grid'
 import styles from '../../styles/GoalTable.module.css'
+import { getCookie } from 'cookies-next'
 
 //Fake Data//
 const JohnGoal1 = {
@@ -59,8 +60,8 @@ const StacyGoal2 = {
 }
 
 const stacy = {
-    _id: 2, 
-    employeeId: 2,  
+    _id: 2,
+    employeeId: 2,
     firstName: 'Stacy',
     lastName: 'Clarke',
     companyId: 2,
@@ -69,8 +70,8 @@ const stacy = {
     goalIDs: [2, 3]
 }
 const john = {
-    _id: 1, 
-    employeeId: 1,  
+    _id: 1,
+    employeeId: 1,
     firstName: 'John',
     lastName: 'Smith',
     companyId: 2,
@@ -79,39 +80,88 @@ const john = {
     goalIDs: [0, 1]
 }
 
+// userform
+// "user": {
+//     "_id": "63448bbbf55d476a2ee14d7b",
+//     "firstName": "Vik",
+//     "lastName": "Singh",
+//     "email": "vdsingh@umass.edu",
+//     "password": "password",
+//     "isManager": true,
+//     "subordinateIDs": [],
+//     "goalIDs": [],
+//     "creationDate": "2022-10-10T21:16:43.463Z",
+//     "lastUpdatedDate": "2022-10-10T21:16:43.463Z",
+//     "lastLoginDate": "2022-10-10T21:16:43.463Z",
+//     "__v": 0
+// }
+
 //The employees you manage:
 const reports = [stacy, john]
 //List of all there goals
 const goals = [JohnGoal1, JohnGoal2, StacyGoal1, StacyGoal2]
 
 function getFullName(params) {
-    const empID = params.getValue(params.id, 'creatorId') 
-    for (const emp of reports){
-        if(empID == emp['employeeId']){
+    const empID = params.getValue(params.id, 'creatorId')
+    for (const emp of reports) {
+        if (empID == emp['employeeId']) {
             return (emp['firstName'] + ' ' + emp['lastName']).toString();
         }
     }
 }
 const columns = [
-    {field: 'fullName', headerName: 'Name', flex: 1, headerClassName: styles.headerLeft, valueGetter: getFullName, filterable: true, sortable: false},
-    {field: 'title', headerName: 'Goal Title', flex: 1, headerClassName: styles.header, filterable: false, sortable: false},
-    {field: 'startDate', headerName: 'Start Date', flex: .5, headerClassName: styles.header, filterable: false, sortable: false},
-    {field: 'endDate', headerName: 'Due By', flex: .5, headerClassName: styles.header, filterable: false, sortable: false},
-    {field: 'status', headerName: 'Status', flex: .5, headerClassName: styles.headerRight, filterable: false, sortable: false},
+    { field: 'fullName', headerName: 'Name', flex: 1, headerClassName: styles.headerLeft, valueGetter: getFullName, filterable: true, sortable: false },
+    { field: 'title', headerName: 'Goal Title', flex: 1, headerClassName: styles.header, filterable: false, sortable: false },
+    { field: 'startDate', headerName: 'Start Date', flex: .5, headerClassName: styles.header, filterable: false, sortable: false },
+    { field: 'endDate', headerName: 'Due By', flex: .5, headerClassName: styles.header, filterable: false, sortable: false },
+    { field: 'status', headerName: 'Status', flex: .5, headerClassName: styles.headerRight, filterable: false, sortable: false },
 ]
 
-export default function MegaTable(){
-    return(
+export default function MegaTable() {
+
+    const router = useRouter()
+    //gets user cookies
+    let userCookie = getCookie('login')
+    try {
+        if (userCookie == undefined) throw "Try signing in again"
+        userCookie = JSON.parse(userCookie)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+    const [goalsData, setTableData] = useState([])
+    const [nameData, setNameData] = useState("")
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/goals/byUserId/" + userCookie.user._id)
+            .then(response => response.json())
+            .then(data => setTableData(data.goals))
+            .catch(error => {
+                console.error("There was an error!", error)
+                alert(error)
+            })
+
+        fetch("http://localhost:3000/api/users/" + userCookie.user._id)
+            .then(response => response.json())
+            .then(data => setNameData(data.user.firstName + " " + data.user.lastName))
+            // .then(data => console.log(data.user.firstName + " " + data.user.lastName))
+            .catch(error => {
+                console.error("There was an error!", error)
+                alert(error)
+            })
+
+    }, [])
+    return (
         <div>
+            {console.log(goalsData)}
             <DataGrid
-            style = {{height:600, width: '90%', margin: 'auto', borderRadius: '20px', backgroundColor: '#81b3b3'}}
-            getRowId={(row) => row._id}
-            rows = {goals}
-            columns = {columns}
-            disableColumnSelector
+                style={{ height: 600, width: '90%', margin: 'auto', borderRadius: '20px', backgroundColor: '#81b3b3' }}
+                getRowId={(row) => row._id}
+                rows={goalsData}
+                columns={columns}
+                disableColumnSelector
             />
         </div>
-
     )
-        
 }
