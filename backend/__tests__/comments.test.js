7bdb2e0e982aa7853de839ee89bad40dde38340e
Antonio Ruiz Fernandez <antonioruiz2606@gmail.com>
2022-11-22 16:15:54 -0500

    Progress

--- backend/__tests__/comments.test.js
+++ backend/__tests__/comments.test.js
@@ -14,10 +14,11 @@ import Goal from "../Models/Goal";
 import User from "../Models/User";
 
 const app = createServer();
-/* Connecting to the database */
-beforeEach(() => {
+/* Connecting to the database and creating test user/goal */
+beforeEach(async () => {
   dotenv.config();
-  mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI);
+  // Making test user
   const testUser = new User({
     firstName: "JestUser",
     lastName: "JestUser",
@@ -25,25 +26,34 @@ beforeEach(() => {
     isManager: true,
     companyId: 0,
     email: "JestUser",
-    password: "JestUser"
+    password: "JestUser",
   });
-  testUser.save();
-  const testGoal = new Goal({
-      title: "JestGoal",
-      description: "JestGoal",
-      goalType: "Performance",
-      status: "Incomplete",
-      priorityValue: 0,
-      startDate: "0-0-0",
-      endDate: "0-0-0",
-      creationDate: "0-0-0",
-      creatorUId: User.findOne({firstName: "JestUser"})._id
-  });
-  testGoal.save();
+  await testUser.save();
+  // Making test goal
+  await User.findOne({ firstName: "JestUser" })
+    .exec()
+    .then((user) => {
+      const testGoal = new Goal({
+        title: "JestGoal",
+        description: "JestGoal",
+        goalType: "Performance",
+        status: "Incomplete",
+        priorityValue: 0,
+        startDate: "0-0-0",
+        endDate: "0-0-0",
+        creationDate: "0-0-0",
+        creatorUId: user._id,
+      });
+      testGoal.save();
+    });
 });
 
-/* Closing database connection after each test. */
+/* Closing database connection after each test and deleting test user/goal */
 afterEach(async () => {
+  // Delete test user
+  await User.deleteOne({ firstName: "JestUser" });
+  // Delete test goal
+  await Goal.deleteOne({ title: "JestGoal" });
   await mongoose.connection.close();
 });
 
@@ -52,15 +62,18 @@ describe("POST /comments", () => {
     // Should respnd with a 200 status code
     // Should respond with a json object that contains a success message and the user object
     // Shold save a comment to the database
-    const response = request(app)
-      .post("/api/comments")
-      .send({
-        description: "JEST Comment",
-        creatorUId: User.findOne({firstName: "JestUser"})._id,
-        goalUId: Goal.findOne({title:"JestGoal"})._id,
-      });
     test("Should respond with a 200 status code", async () => {
-        expect(response.statusCode).toBe(200);
+      await User.findOne({ firstName: "JestUser" }).then(async (user) => {
+        await Goal.findOne({ title: "JestGoal" }).then(async (goal) => {
+          const response = await request(app).post("/api/comments").send({
+            description: "JestComment",
+            creatorUId: user._id,
+            goalUId: goal._id,
+          });
+          expect(response.statusCode).toBe(200);
+          await Comment.deleteOne({ description: "JestComment" });
+        });
+      });
     });
     // test("Should respond with a JSON object that contains a success message and the user object", () => {
     //     expect(response.body.message).toBe("Successfully created Comment.");
