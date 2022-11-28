7ca2439ef6d770fda0e7f91613c75b3983d0edec
unknown <chloex2000@gmail.com>
2022-11-27 21:45:37 -0500

    Shows no manager if user has no manager by checking managerId.

--- frontend/pages/components/ManagerInfo.js
+++ frontend/pages/components/ManagerInfo.js
@@ -3,22 +3,42 @@ import styles from '../../styles/employee.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-var testMan1 = {
-    name: 'Hai Nguyen',
-    position_title: 'Principal Engineering Manager'
+const getManagerInfo = () =>{
+    //placeholder
+    var [loginInfo, setLoginInfo] = useState({user: {firstName: "", lastnName: "", position_title: ''}})
+    if (getCookie("login") != undefined){
+        useEffect(() => setLoginInfo(JSON.parse(getCookie("login"))), [])
+        //var loginInfo = JSON.parse(getCookie("login"))
+    }
+
+    var man = {
+        hasMan: loginInfo.user.managerId,
+        firstname: loginInfo.user.firstName,
+        lastname: loginInfo.user.lastName,
+        fullname: loginInfo.user.firstName +" "+ loginInfo.user.lastName,
+        position_title: loginInfo.user.positionTitle
+    }
+    return man
 }
 
 export default function EmployeeName(){
     const router = useRouter()
-
-    return(
-        <div>
-            <div className={styles.title} id={styles.manager_title}> Your Manager </div>
-            <div className={styles.manager_box}>
-                <h1 className={styles.emp_name}>{testMan1.name}</h1>
-                <h2 className={styles.job_title}>{testMan1.position_title}</h2>
+    if (getManagerInfo.hasMan) {
+        return(
+            <div>
+                <div className={styles.title} id={styles.manager_title}> Your Manager </div>
+                    <div className={styles.manager_box}>
+                        <h1 className={styles.emp_name}>{man.fullname}</h1>
+                        <h2 className={styles.job_title}>{man.position_title}</h2>
+                    </div>
             </div>
-        </div>
-       
-    )
+        )
+    } else {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+
 }
