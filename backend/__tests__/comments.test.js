a5bf269d869c6417810f9084374bc2e622870005
Antonio <antonioruiz2606@gmail.com>
2022-11-29 09:47:38 -0500

    Done with some CRUDS

--- backend/__tests__/comments.test.js
+++ backend/__tests__/comments.test.js
@@ -43,23 +43,40 @@ beforeEach(async () => {
     creationDate: "0-0-0",
     creatorUId: user._id,
   });
+  // Making test comments
+  const goal = await Goal.findOne({ title: "JestGoal" }).exec()
+  const testComment1 = new Comment({
+    description: "JestComment1",
+    creatorUId: user._id,
+    goalUId: goal._id
+  });
+  const testComment2 = new Comment({
+    description: "JestComment2",
+    creatorUId: user._id,
+    goalUId: goal._id
+  });
+  const testComment3 = new Comment({
+    description: "JestComment1",
+    creatorUId: user._id,
+    goalUId: goal._id
+  });
   await testGoal.save()
 });
 
 /* Closing database connection after each test and deleting test user/goal */
 afterEach(async () => {
   // Delete test user
-  await User.deleteOne({ firstName: "JestUser" });
+  await User.remove({ firstName: "JestUser" });
   // Delete test goal
-  await Goal.deleteOne({ title: "JestGoal" });
+  await Goal.remove({ title: "JestGoal" });
+  // Delete test comments
+  
   await mongoose.connection.close();
 });
 
 describe("POST /comments", () => {
   describe("given a comment object with required fields", () => {
     // Should respond with a 200 status code
-    // Should respond with a json object that contains a success message and the user object
-    // Shold save a comment to the database
     test("Should respond with a 200 status code", async () => {
       const user = await User.findOne({ firstName: "JestUser" }).exec();
       const goal = await Goal.findOne({ title: "JestGoal" }).exec();
@@ -69,10 +86,11 @@ describe("POST /comments", () => {
         goalUId: goal._id,
       });
       expect(response.statusCode).toBe(200);
-      await Comment.deleteOne({ description: "JestComment" });
+      await Comment.remove({ description: "JestComment" });
 
     });
-    test("Should respond with a JSON object that contains a success message and the user object", async () => {
+    // Should respond with a JSON object that contains a success message and the comment object
+    test("Should respond with a JSON object that contains a success message and the comment object", async () => {
         const user = await User.findOne({ firstName: "JestUser" }).exec();
         const goal = await Goal.findOne({ title: "JestGoal" }).exec();
         const response = await request(app).post("/api/comments").send({
@@ -83,13 +101,26 @@ describe("POST /comments", () => {
         expect(response.body.message).toBe("Successfully created Comment.");
         expect(response.body.comment).toMatchObject({
             description: "JestComment",
-            creatorUId: await User.findOne({firstName: "JestUser"}).exec()._id,
-            goalUId: await Goal.findOne({title:"JestGoal"}).exec()._id,
+            creatorUId: user._id,
+            goalUId: goal._id,
           });
+        await Comment.remove({ description: "JestComment" });
     });
-    // test("Should save a comment to the database", () => {
-    //     const comment = Comment.find({description: "JEST Comment"});
-    //     expect(comment.length).not.toBe(0);
-    // });
+    // Should add a comment to the database
+    test("Should add a comment to the database", async () => {
+      const user = await User.findOne({ firstName: "JestUser" }).exec();
+      const goal = await Goal.findOne({ title: "JestGoal" }).exec();
+      const response = await request(app).post("/api/comments").send({
+        description: "JestComment",
+        creatorUId: user._id,
+        goalUId: goal._id,
+      });
+      const comment = await User.findOne({ description: "JestComment" }).exec()
+      expect(comment).not.toBeError
+      
+      await Comment.remove({ description: "JestComment" });
+  });
   });
 });
+
+describe("GET /comments/byGoalId/:goalId")
