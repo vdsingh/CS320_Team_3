c3cade70fa2a31321796d6d71472f3cff9237a1c
Chloe X <chloex2000@gmail.com>
2022-11-29 16:56:05 -0500

    new branch for manager box

--- frontend/pages/components/ManagerInfo.js
+++ frontend/pages/components/ManagerInfo.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../../styles/employee.module.css'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { getCookie } from 'cookies-next'
+
 
 var testMan1 = {
     name: 'Hai Nguyen',
@@ -11,12 +13,32 @@ var testMan1 = {
 export default function EmployeeName(){
     const router = useRouter()
 
+    let userCookie = getCookie('login')
+    try {
+        if (userCookie == undefined) throw "Try signing in again"
+        userCookie = JSON.parse(userCookie)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+    const [manager, setTableData] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:3000/api/users/getUser/"+userCookie.user.managerId+"/"+userCookie.user.companyId)
+        .then(response => response.json())
+        .then(data => setTableData(data.user))
+        .catch(error => {
+            console.error("There was an error!", error)
+            alert(error)
+        })
+    }, [])
+    console.log(manager)
     return(
         <div>
             <div className={styles.title} id={styles.manager_title}> Your Manager </div>
             <div className={styles.manager_box}>
-                <h1 className={styles.emp_name}>{testMan1.name}</h1>
-                <h2 className={styles.job_title}>{testMan1.position_title}</h2>
+                <h1 className={styles.emp_name}>{manager.firstName + " " + manager.lastName}</h1>
+                <h2 className={styles.job_title}>{manager.position_title}</h2>
             </div>
         </div>
        
