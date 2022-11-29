4452531b3067bb64370be6279cac447ed2387f83
Chloe X <chloex2000@gmail.com>
2022-11-29 16:34:37 -0500

    commiting broken code will come back to later

--- frontend/pages/components/ManagerInfo.js
+++ frontend/pages/components/ManagerInfo.js
@@ -6,39 +6,62 @@ import { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 const getManagerInfo = () =>{
-    var [loginInfo, setLoginInfo] = useState({user: {firstName: "", lastnName: "", position_title: ''}})
-    if (getCookie("login") != undefined){
-        useEffect(() => setLoginInfo(JSON.parse(getCookie("login"))), [])
+    // old implementation that didn't work for people with managers T_T
+
+    // var [loginInfo, setLoginInfo] = useState({user: {firstName: "", lastnName: "", position_title: ''}})
+    // if (getCookie("login") != undefined){
+    //     useEffect(() => setLoginInfo(JSON.parse(getCookie("login"))), [])
+    // }
+    // var man = {
+    //     hasMan: loginInfo.user.managerId,
+    //     firstname: loginInfo.user.firstName,
+    //     lastname: loginInfo.user.lastName,
+    //     fullname: loginInfo.user.firstName +" "+ loginInfo.user.lastName,
+    //     position_title: loginInfo.user.positionTitle
+    // }
+    // return man
+
+    // new implementation trying to fetch things
+    let userCookie = getCookie('login')
+    try {
+        if (userCookie == undefined) throw "Try signing in again"
+        userCookie = JSON.parse(userCookie)
+    }
+    catch (err) {
+        console.log(err)
     }
 
-    var man = {
-        hasMan: loginInfo.user.managerId,
-        firstname: loginInfo.user.firstName,
-        lastname: loginInfo.user.lastName,
-        fullname: loginInfo.user.firstName +" "+ loginInfo.user.lastName,
-        position_title: loginInfo.user.positionTitle
-    }
-    return man
+    const [manager, setTableData] = useState([])
+    useEffect(() => {
+        // what do i fetch? 
+        // i ideally want to be able to call something like manager.firstName and manager.positionTitle
+        fetch("http://localhost:3000/api/users/"+userCookie.user.employeeId+"/"+userCookie.user.companyId)
+        .then(response => response.json())
+        .then(data => setTableData(data.user))
+        .catch(error => {
+            console.error("There was an error!", error)
+            alert(error)
+        })
+    }, [])
+    return manager
 }
 
 export default function EmployeeName(){
     const router = useRouter()
-    if (getManagerInfo().hasMan) {
+    // what do i put in the conditional?
+    if (getManagerInfo().firstName) {
         return(
             <div>
                 <div className={styles.title} id={styles.manager_title}> Your Manager </div>
                     <div className={styles.manager_box}>
-                        <h1 className={styles.emp_name}>{getManagerInfo().fullname}</h1>
-                        <h2 className={styles.job_title}>{getManagerInfo().position_title}</h2>
+                        <h1 className={styles.emp_name}>{getManagerInfo().firstName + " " + getManagerInfo().lastName}</h1>
+                        <h2 className={styles.job_title}>{getManagerInfo().positionTitle}</h2>
                     </div>
             </div>
         )
     } else {
-        return(
-            <div>
-                
-            </div>
+        return (
+            <div></div>
         )
     }
-
 }
