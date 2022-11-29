3864a893c254b05437f978cefe0a5ee1399b33b3
Willow <113944736+willow-lafaso@users.noreply.github.com>
2022-11-29 10:00:34 -0500

    Checks on login and during nav for is Manager

--- frontend/pages/pages/your-goal.js
+++ frontend/pages/pages/your-goal.js
@@ -6,10 +6,11 @@ import styles from '../../styles/goal.module.css'
 import styles2 from '../../styles/popup.module.css'
 import { useRouter } from 'next/router'
 import EditGoalPopup from '../components/EditGoalPopup'
+import { getCookie } from 'cookies-next';
 
 export default function goalPage(){
     const router = useRouter()
-
+    const navbar = JSON.parse(getCookie('login')).user.isManager ? 4 : 3;
     const [testGoal, setTableData] = useState([])
     useEffect(() => {
         fetch("http://localhost:3000/api/goals/byGoalId/"+router.query.id)
@@ -23,7 +24,7 @@ export default function goalPage(){
 
     if (loginValidator()){
         return(
-            <Layout navbarType={3}>
+            <Layout navbarType={navbar}>
                 <div>
                     <Head>
                         <title>Employee Page</title>
