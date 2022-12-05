7076e56078272aaa6ddb2aceb6a6f4c7436eb981
Quynh <qdpham@umass.edu>
2022-12-05 11:05:28 -0500

    Edit goal popup integration

--- frontend/pages/components/EditGoalPopup.js
+++ frontend/pages/components/EditGoalPopup.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 import styles from '../../styles/popup.module.css'
 import Router from 'next/router'
 import styles2 from '../../styles/employee.module.css'
-import { getCookie } from 'cookies-next'
+
 const customStyles = {
     content: {
         top: '50%',
@@ -21,39 +21,8 @@ const customStyles = {
 if (typeof document !== 'undefined') {
     Modal.setAppElement(document.getElementById('root'));
 }
-const getCurrentGoal = () => {
-    //API integration goes here to get the current goal
-    var goalName = 'Goal name'
-    var goalDescription = 'Goal description'
-    var startDate = "2023-10-23"
-    var dueDate ="2023-10-23"
-    var goalTypes = 'Developmental'
-    var status = 'Incomplete'
-    return {goalName: goalName, startDate: startDate, dueDate: dueDate, goalTypes: goalTypes, status: status, goalDescription: goalDescription}
-}
-const submit = async (event) => {
-    event.preventDefault()
-    const goalName = event.target.goalName.value
-    //const currentTime =  new Date()
-    const startDate = new Date(event.target.startDate.value.concat("T00:00:00"))
-    const status = event.target.status.value
-    const dueDate = new Date(event.target.dueDate.value.concat("T23:59:59"))
-    const goalDescription = event.target.goalDescription.value
-    const goalType = event.target.goalType.value
 
-    // Client-side check of password and email validity
-    if (goalName == '' || dueDate == '' || goalDescription == '' || goalType == '') {
-        alert('Please fill all the boxes')
-    }
-    else if (startDate > dueDate) {
-        alert('Due Date cannot be earlier than the start Date')
-    }
-
-    else {
-    }
-}
-
-function EditGoalPopup() {
+function EditGoalPopup({thisGoal}) {
     let subtitle;
     const [modalIsOpen, setIsOpen] = React.useState(false);
 
@@ -72,6 +41,61 @@ function EditGoalPopup() {
         setIsOpen(false);
     }
 
+    const submit = async (event) => {
+        event.preventDefault()
+        const goalName = event.target.goalName.value
+        //const currentTime =  new Date()
+        const startDate = new Date(event.target.startDate.value.concat("T00:00:00"))
+        const status = event.target.status.value
+        const dueDate = new Date(event.target.dueDate.value.concat("T23:59:59"))
+        const goalDescription = event.target.goalDescription.value
+        const goalType = event.target.goalType.value
+    
+        // Client-side check of password and email validity
+        if (goalName == '' || dueDate == '' || goalDescription == '' || goalType == '') {
+            alert('Please fill all the boxes')
+        }
+        else if (startDate > dueDate) {
+            alert('Due Date cannot be earlier than the start Date')
+        }
+    
+        else {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    title: goalName,
+                    description: goalDescription,
+                    goalType: goalType,
+                    status: status
+                })
+            }
+            // API request to the login API
+            fetch('http://localhost:3000/api/goals/byGoalId/'+thisGoal._id, requestOptions)
+                .then(async response => {
+                    const isJson = response.headers.get('content-type')?.includes('application/json');
+                    const data = isJson && await response.json();
+                    console.log(data);
+                    // check for error response
+                    if (!response.ok) {
+                        // get error message from body or default to response status
+                        const error = (data && data.message) || response.status;
+                        return Promise.reject(error);
+                    }
+                    // If there is no error, proceed to the next page
+                    else {
+                        alert(data.message);
+                        console.log(data)
+                        Router.reload(window.location.pathname)
+                    }
+                })
+                .catch(error => {
+                    console.error('There was an error!', error);
+                    alert(error);
+                });
+        }
+    }
+
     return (
         <a>
             <button className={styles2.button} onClick={openModal}>Edit Goal</button>
@@ -86,28 +110,28 @@ function EditGoalPopup() {
                 <div className={styles.heading}>Edit your goal</div>
                 <form onSubmit={submit}>
                     <div className={styles.text}>Name of Goal:</div>
-                    <input id='goalName' type='text' defaultValue= {getCurrentGoal().goalName} className={styles.input} required></input>
+                    <input id='goalName' type='text' defaultValue= {thisGoal.title} className={styles.input} required></input>
                     <span>
                         <div>
                             <a className={styles.text}>Start Date:</a>
                             <a className={styles.text2}>Due Date:</a>
                         </div>
-                        <input id='startDate' type='date' defaultValue={getCurrentGoal().startDate} className={styles.inputDate} required></input>
-                        <input id='dueDate' type='date' defaultValue={getCurrentGoal().dueDate} className={styles.inputDate} required ></input>
+                        <input id='startDate' type='date' defaultValue={thisGoal.startDate} className={styles.inputDate} required></input>
+                        <input id='dueDate' type='date' defaultValue={thisGoal.endDate} className={styles.inputDate} required ></input>
                     </span>
                     <div className={styles.text}>Goal Description:</div>
-                    <input id='goalDescription' type='text' defaultValue={getCurrentGoal().goalDescription} className={styles.input} required></input>
+                    <input id='goalDescription' type='text' defaultValue={thisGoal.description} className={styles.input} required></input>
                     <span>
                         <div>
                             <a className={styles.text}>Type of Goal:</a>
                             <a className={styles.text3}>Status:</a>
                         </div>
-                        <select name="goalTypes" id="goalType" className={styles.inputDate} defaultValue={getCurrentGoal().goalTypes} required>
+                        <select name="goalTypes" id="goalType" className={styles.inputDate} defaultValue={thisGoal.goalType} required>
                             <option value="Performance">Performance</option>
                             <option value="Developmental">Developmental</option>
                             <option value="Personal">Personal</option>
                         </select>
-                        <select name="status" id="status" className={styles.inputDate} defaultValue={getCurrentGoal().status} required>
+                        <select name="status" id="status" className={styles.inputDate} defaultValue={thisGoal.status} required>
                             <option value="In progress">In progress</option>
                             <option value="Incomplete">Incomplete</option>
                             <option value="Completed">Completed</option>
